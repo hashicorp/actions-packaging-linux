20b4867e6be3c2c91cf8c91f35b03f8232d9ae72
Kyle Penfound <kpenfound11@gmail.com>
2022-02-28 13:57:38 -0500

    ensure out directory exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ INPUT_DEPENDS=$INPUT_RPM_DEPENDS /usr/local/bin/nfpm_template > ./nfpm_rpm_confi
 INPUT_DEPENDS=$INPUT_DEB_DEPENDS /usr/local/bin/nfpm_template > ./nfpm_deb_config.yaml
 
 echo "Packaging..."
+mkdir -p ./out
 /usr/local/bin/nfpm package -f ./nfpm_rpm_config.yaml -p rpm -t ./out/
 /usr/local/bin/nfpm package -f ./nfpm_deb_config.yaml -p deb -t ./out/
