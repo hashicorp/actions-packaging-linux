d83bf6f35fdce6b4b1e99963517dc6c321ec1378
Claire <claire@hashicorp.com>
2023-02-02 12:42:15 -0600

    PR feedback

--- fpm_template.go
+++ fpm_template.go
@@ -76,8 +76,6 @@ func main() {
 	inputName := os.Getenv("INPUT_NAME")
 	inputArch := os.Getenv("INPUT_ARCH")
 	inputVersion := os.Getenv("INPUT_VERSION")
-	inputMaintainer := os.Getenv("INPUT_MAINTAINER")
-	inputVendor := os.Getenv("INPUT_VENDOR")
 	inputDescription := os.Getenv("INPUT_DESCRIPTION")
 	inputHomepage := os.Getenv("INPUT_HOMEPAGE")
 	inputLicense := os.Getenv("INPUT_LICENSE")
@@ -97,6 +95,15 @@ func main() {
 	binName := filepath.Base(inputBinary)
 	binDest := filepath.Join(inputBinPath, binName)
 
+	inputVendor := os.Getenv("INPUT_VENDOR")
+	if inputVendor == "" {
+		inputVendor = "HashiCorp"
+	}
+
+	inputMaintainer := os.Getenv("INPUT_MAINTAINER")
+	if inputVendor == "" {
+		inputVendor = "HashiCorp"
+	}
 	// This maps to "armv7hl" for rpm and "armhf" for deb
 	// "arm" is not a valid arch for either type, and it
 	// does not get mapped automatically in nfpm
@@ -139,8 +146,8 @@ arch: {{ .Arch }}
 platform: linux
 release: 1
 version: {{ .Version }}
-maintainer: "HashiCorp"
-vendor: "HashiCorp"
+maintainer: {{ .Maintainer}}
+vendor: {{ .Vendor }}
 description: {{ .Description }}
 homepage: {{ .Homepage }}
 license: {{ .License }}
