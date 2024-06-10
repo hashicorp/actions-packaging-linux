81c4f37080929daf6710adb7d130ad88ad39a8b5
Brian Shore <bshore@hashicorp.com>
2024-06-10 15:37:24 -0700

    Add support for specifying file ownership inside a package

--- fpm_template.go
+++ fpm_template.go
@@ -27,6 +27,8 @@ type NfpmInput struct {
 	Postinstall     string
 	Preremove       string
 	Postremove      string
+	UserOwner       string
+	GroupOwner      string
 	FilePermissions string
 
 	ConfigFiles []*ConfigFile
@@ -92,6 +94,8 @@ func main() {
 	inputPreremove := os.Getenv("INPUT_PREREMOVE")
 	inputPostremove := os.Getenv("INPUT_POSTREMOVE")
 	inputPermissions := os.Getenv("INPUT_FILEPERMISSIONS")
+	inputUserOwner := os.Getenv("INPUT_USEROWNER")
+	inputGroupOwner := os.Getenv("INPUT_GROUPOWNER")
 
 	depends := strings.Split(inputDepends, ",")
 	if inputDepends == "" {
@@ -127,6 +131,8 @@ func main() {
 		Preremove:       inputPreremove,
 		Postremove:      inputPostremove,
 		FilePermissions: inputPermissions,
+		UserOwner:       inputUserOwner,
+		GroupOwner:      inputGroupOwner,
 	}
 
 	input.ConfigFiles = findConfigs(inputConfigDir)
@@ -163,12 +169,33 @@ contents:
 {{- if ne .Binary "" }}
   - src: {{ .Binary }}
     dst: {{ .BinaryDest }}
+{{- if or (ne .UserOwner "") (ne .GroupOwner "") }}
+    file_info:
+{{- if ne .UserOwner "" }}
+      owner: root
 {{- end }}
+{{- if ne .GroupOwner "" }}
+      group: vault
+{{- end }}
+{{- end }}
+{{- end }}
+{{- /* capture ownership for use in .ConfigFiles subcontext */ -}}
+{{- $userOwner := .UserOwner }}
+{{- $groupOwner := .GroupOwner }}
 {{- with .ConfigFiles }}
 {{- range $index, $element := . }}
   - src: {{ .LocalPath }}
     dst: {{ .DestPath }}
     type: config|noreplace
+{{- if or (ne $userOwner "") (ne $groupOwner "") }}
+    file_info:
+{{- if ne $userOwner "" }}
+      owner: root
+{{- end }}
+{{- if ne $groupOwner "" }}
+      group: vault
+{{- end }}
+{{- end }}
 {{- end }}
 {{- end }}
 scripts:
